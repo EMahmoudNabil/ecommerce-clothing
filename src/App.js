ef169903d2eb311ceb502796b643724000c753e5
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2022-03-02 11:10:11 +0200

    adding component  and links to home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './components/shop/shop.component';
+import Header from './components/header/header.component.jsx'
 
 
 
@@ -12,7 +13,9 @@ function App() {
   return (
     
   <div>
+      <Header />
   <Switch>
+
     <Route  exact path='/' component={HomePage}/>
     <Route   path='/shop-page' component={ShopPage}/>
 
