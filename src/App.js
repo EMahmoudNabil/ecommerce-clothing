2d19c893a079dbb3d6411141701c0c883963bb79
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2022-03-03 10:32:53 +0200

    how to control user in firebase singin

--- src/App.js
+++ src/App.js
@@ -5,32 +5,59 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
+import { auth } from './firebase/firebase.utils';
 
 
 
 
+class App extends React.Component {
 
-function App() {
-  return (
+  constructor(){
+    super();
+
+    this.state={
+      currentUser :null
+    }
+
+  }
+
+unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+      this.setState( {currentUser :user});
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
     
-  <div>
-      <Header />
-  <Switch>
-
-    <Route  exact path='/' component={HomePage}/>
-    <Route   path='/shop' component={ShopPage}/>
-    <Route   path='/signin' component={SignInAndSignUp}/>
-
-
-  </Switch>
+      <div>
+          <Header  />
+      <Switch>
+    
+        <Route  exact path='/' component={HomePage}/>
+        <Route   path='/shop' component={ShopPage}/>
+        <Route   path='/signin' component={SignInAndSignUp}/>
+    
+    
+      </Switch>
+         
+    
+      </div>
+        
      
-
-  </div>
+          
+       
+      );
+    }
     
- 
-      
-   
-  );
-}
 
+  }
+  
 export default App;
