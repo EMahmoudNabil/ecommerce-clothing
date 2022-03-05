349f4d35cce684c39b1caee4955e498d12c6d916
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2022-03-05 11:36:16 +0200

    adding cart icon to header and styles it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,8 @@ import {ReactComponent as Logo} from '../../assets/084 crown.svg'
 import './header.styles.scss';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+
 
 
 
@@ -22,6 +24,8 @@ const Header =({currentUser})=>(
                 :
                 <Link className='option' to='/signin'> SIGN IN</Link>
             }
+
+            <CartIcon />
         </div>
     </div>
 )
