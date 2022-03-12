4219930dce2b485b750656fd41d4bd255b42f1c9
Mahmoud Nabil <e.mahmoudnabil@gmail.com>
2022-03-12 22:22:01 +0200

    Data Normalization + Collection Page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -20,7 +20,5 @@ export const selectCollections = createSelector(
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
-    collections => collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-  )
+    collections => collections[collectionUrlParam]
   );
